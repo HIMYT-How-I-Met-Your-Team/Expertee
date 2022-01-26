c5bc8a33938da5866bbf2e93946cb10a2f9b3024
BadNarrators <andreaatzei01@gmail.com>
2022-01-27 00:20:45 +0100

    Correct redirects in header.php

--- header.php
+++ header.php
@@ -9,11 +9,11 @@
 <header>
     <div id="brand"><a href="#"><img src="<?php echo get_stylesheet_directory_uri();?>/img/Logo_expertee_2.svg" alt="logo expertee"></a></div>
         <div id="nav">
-            <a href="#contattaci"><span id="contattaci">CONTATTACI</span></a>
-            <a href="#chisiamo" >CHI SIAMO</a>
-            <a href="#incloud">INCLOUD</a>
-            <a href="#software">SOFTWARE</a>
-            <a href="#home">HOMEPAGE</a>
+            <a href="index.php/contattaci"><span id="contattaci">CONTATTACI</span></a>
+            <a href="index.php/chisiamo" >CHI SIAMO</a>
+            <a href="index.php/incloud">INCLOUD</a>
+            <a href="index.php/software">SOFTWARE</a>
+            <a href="index.php/home">HOMEPAGE</a>
         </div>
         <div id="hamburger-menu" onclick="toggleMobileMenu(this)">
             <div  id="hamburger-icon">
