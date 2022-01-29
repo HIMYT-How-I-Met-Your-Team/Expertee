babdb31b8ff8f564e633a1f17c255d50bec96bff
Sgulli <sguglielmi40@gmail.com>
2022-01-29 19:28:18 +0100

    Update functions.php
    include "pages/form.php";

--- functions.php
+++ functions.php
@@ -2,10 +2,11 @@
 
 // default and necessary function to make the child-theme work
 
-function twentyonechild_theme_enqueue_styles() {
-   wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-         array( 'parenthandle' ), 
-         wp_get_theme()->get('Version') // this only works if you have Version in the style header
+function twentyonechild_theme_enqueue_styles()
+{
+   wp_enqueue_style('child-style',get_stylesheet_uri(),
+      array('parenthandle'),
+      wp_get_theme()->get('Version') // this only works if you have Version in the style header
    );
 }
 
@@ -15,51 +16,56 @@ function twentyonechild_theme_enqueue_styles() {
 // ________________
 
 // prints the necessary code to include a CSS style from the filename
-function get_style($styleName){
-   echo("<link rel='stylesheet' href='".get_stylesheet_directory_uri()."/styles/".$styleName.".css'>");
+function get_style($styleName)
+{
+   echo ("<link rel='stylesheet' href='" . get_stylesheet_directory_uri() . "/styles/" . $styleName . ".css'>");
 }
 
 // prints the necessary code to include a JS script from the filename
-function get_script($scriptName){
-   echo '<script type="text/javascript" src="'.get_stylesheet_directory_uri().'/scripts/'.$scriptName.'.js"></script>';
+function get_script($scriptName)
+{
+   echo '<script type="text/javascript" src="' . get_stylesheet_directory_uri() . '/scripts/' . $scriptName . '.js"></script>';
 }
 
 
 
 // includes the required CSS styles and JS scripts and initializes the page
-function get_head($styleArray, $scriptArray){
-   foreach ($styleArray as $style){
+function get_head($styleArray, $scriptArray)
+{
+   foreach ($styleArray as $style) {
       get_style($style);
    }
-   foreach ($scriptArray as $script){
-      get_script($script); 
+   foreach ($scriptArray as $script) {
+      get_script($script);
    }
 }
 
 // includes the required JS scripts
-function get_finish_scripts($scriptArray){
-   foreach ($scriptArray as $script){
-      get_script($script); 
+function get_finish_scripts($scriptArray)
+{
+   foreach ($scriptArray as $script) {
+      get_script($script);
    }
 }
 
 // prints the body part from the name of its file (there can be more than one)
-function get_body($pageName){
-   include "pages/".$pageName.".php";
+function get_body($pageName)
+{
+   include "pages/" . $pageName . ".php";
 }
 
 // prints the form 
-function get_form(){
-   include "form.php";
+function get_form()
+{
+   include "pages/form.php";
 }
 
 // // inserts data inside the database
-function insert($data){
-   if (function_exists('inserter'))
-   {
+function insert($data)
+{
+   if (function_exists('inserter')) {
       inserter($data);
-   }
-   else {
-      echo("<h2>You're missing the plugin 'lead-view' for the form management.</h2>");
+   } else {
+      echo ("<h2>You're missing the plugin 'lead-view' for the form management.</h2>");
    }
 }
